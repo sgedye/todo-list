1fab2feacf36cb00254ba1ff09f45b14ec723153
Shaun Gedye <sgedye88@yahoo.com.au>
2020-06-10 14:23:56 +0800

    Updates

---
+++ precache-manifest.346782c363564929e9fbc606ee068bd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e0ca6eb87b24137b73210b1ec34a1f",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "60dcc56780b2cf3aeca2",
+    "url": "/todo-list/static/css/main.c8a57424.chunk.css"
+  },
+  {
+    "revision": "8a98cc772f11d3d3354b",
+    "url": "/todo-list/static/js/2.78205d7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.78205d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60dcc56780b2cf3aeca2",
+    "url": "/todo-list/static/js/main.4d7fdc9b.chunk.js"
+  },
+  {
+    "revision": "2b909f8b49d22c3e9c72",
+    "url": "/todo-list/static/js/runtime-main.5e1967fb.js"
+  }
+]);
